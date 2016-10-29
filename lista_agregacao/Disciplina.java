ae5646e7fb74b81279dc46c9b7693c1e9beaca4a
João Manoel <joaomanoel7@gmail.com>
2016-10-28 22:19:29 -0200

    update repo ...

--- lista_agregacao/Disciplina.java
+++ lista_agregacao/Disciplina.java
@@ -2,4 +2,10 @@ package lista_agregacao;
 
 public class Disciplina {
 
+    private String nomeDisciplina;
+    private int creditos;
+    private int cargaHoraria;
+    private int codProfessor;
+
+
 }
--- lista_agregacao/Professor.java
+++ lista_agregacao/Professor.java
@@ -2,4 +2,55 @@ package lista_agregacao;
 
 public class Professor {
 
+    private String nome;
+    private String cpf;
+
+    /**
+     * @param nome
+     * @param cpf
+     */
+    public Professor(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    /* @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    } */
 }
