ea5c75f6f5225317fcfc4c3254e05ce92f5631d1
João Manoel <joaomanoel7@gmail.com>
2016-10-29 03:15:01 -0200

    update Disciplina.java ...

--- lista_agregacao/Disciplina.java
+++ lista_agregacao/Disciplina.java
@@ -5,7 +5,49 @@ public class Disciplina {
     private String nomeDisciplina;
     private int creditos;
     private int cargaHoraria;
-    private int codProfessor;
 
+    /**
+     * @param nomeDisciplina
+     * @param creditos
+     * @param cargaHoraria
+     */
+    public Disciplina(String nomeDisciplina, int creditos, int cargaHoraria) {
+        this.nomeDisciplina = nomeDisciplina;
+        this.creditos = creditos;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    /**
+     * @return the nomeDisciplina
+     */
+    public String getNomeDisciplina() {
+        return nomeDisciplina;
+    }
+
+    /**
+     * @return the creditos
+     */
+    public int getCreditos() {
+        return creditos;
+    }
+
+    /**
+     * @return the cargaHoraria
+     */
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
 
 }
