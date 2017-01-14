c592cac262a98e6c165efca04d824b8381712013
João Manoel <joaomanoel7@gmail.com>
2017-01-14 10:46:21 -0200

    update DVD.java [JDEE]

--- lista_agregacao/DVD.java
+++ lista_agregacao/DVD.java
@@ -1,70 +1,85 @@
 package lista_agregacao;
 
 public class DVD {
-	private String marca;
-	private String modelo;
+    private String marca;
+    private String modelo;
 
-	public DVD(String marca, String modelo) {
-		this.marca = marca;
-		this.modelo = modelo;
-	}
+    public DVD(String marca, String modelo) {
+	this.marca = marca;
+	this.modelo = modelo;
+    }
 
-	/**
-	 * get the value of marca
-	 * @return the value of marca
-	 */
-	public String getMarca(){
-		return this.marca;
-	}
+    /**
+     * get the value of marca
+     * @return the value of marca
+     */
+    public String getMarca(){
+	return this.marca;
+    }
 
-	/**
-	 * set a new value to marca
-	 * @param marca the new value to be used
-	 */
-	public void setMarca(String marca) {
-		this.marca=marca;
-	}
+    /**
+     * set a new value to marca
+     * @param marca the new value to be used
+     */
+    public void setMarca(String marca) {
+	this.marca=marca;
+    }
 
-	/**
-	 * get the value of modelo
-	 * @return the value of modelo
-	 */
-	public String getModelo(){
-		return this.modelo;
-	}
+    /**
+     * get the value of modelo
+     * @return the value of modelo
+     */
+    public String getModelo(){
+	return this.modelo;
+    }
 
-	/**
-	 * set a new value to modelo
-	 * @param modelo the new value to be used
-	 */
-	public void setModelo(String modelo) {
-		this.modelo=modelo;
-	}
+    /**
+     * set a new value to modelo
+     * @param modelo the new value to be used
+     */
+    public void setModelo(String modelo) {
+	this.modelo=modelo;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DVD other = (DVD) obj;
-		if (marca == null) {
-			if (other.marca != null)
-				return false;
-		} else if (!marca.equals(other.marca))
-			return false;
-		if (modelo == null) {
-			if (other.modelo != null)
-				return false;
-		} else if (!modelo.equals(other.modelo))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	DVD other = (DVD) obj;
+	if (marca == null) {
+	    if (other.marca != null)
+		return false;
+	} else if (!marca.equals(other.marca))
+	    return false;
+	if (modelo == null) {
+	    if (other.modelo != null)
+		return false;
+	} else if (!modelo.equals(other.modelo))
+	    return false;
+	return true;
+    }
 
-	@Override
-	public String toString() {
-		return "DVD [marca=" + marca + ", modelo=" + modelo + "]";
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public String toString() {
+	final int sbSize = 1000;
+	final String variableSeparator = "  ";
+	final StringBuffer sb = new StringBuffer(sbSize);
+
+	sb.append("marca=").append(marca);
+	sb.append(variableSeparator);
+	sb.append("modelo=").append(modelo);
+
+	return sb.toString();
+    }
+
+    //@Override
+    //	public String toString() {
+    //		return "DVD [marca=" + marca + ", modelo=" + modelo + "]";
 }
+
